7f0dc0d94631a9b0a32a85117a8ca85193655794
BBAvants <stnava@gmail.com>
2014-01-10 23:58:38 -0500

    WIP: rendering example

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ RenderBrain
 ===========
 
 starter project for rendering brains automatically
+
+some things need to be changed / tuned by hand
+
+these are noted in the file renderBrain.sh by #Tweak
+
+e.g. orientation of the image you want to register 
+
+axis to slice and other stuff ...
+
+` ./renderBrain3.sh brain08.nii.gz brain08seg.nii.gz RefOutput 140 2 `
+
+gives you output starting with RefOutput* ( see included reference output ).
--- renderBrain3.sh
+++ renderBrain3.sh
@@ -1,33 +1,34 @@
-echo $#
-if [[ $# -lt 3 ]]
+#!/bin/bash
+if [[ $# -lt 5 ]]
 then
-  echo Usage: renderBrain.sh input_brain.nii.gz input_segmentation.nii.gz  axial_slice_number   input_affine.mat  input_template.nii.gz  output_prefix
-  echo Note: input_segmentation is a 0/1 binary image. if no affine is needed, use \"identity.mat\" as input_affine
+  echo Usage: renderBrain.sh input_brain.nii.gz input_mask.nii.gz output_prefix axial_slice_number axistoslice
+  echo Note: input_segmentation is a 0/1 binary image. 
   exit
 fi 
-
-input_brain=$1
-input_seg=$2  # 0/1 binary segmentation, e.g. GM or WM+GM
-output_prefix=$3
-axial_slice_number=$4
-input_affine=$5
-input_template=$6
-
-vtkVol=${output_prefix}.vtk
+input_brain=$1    # raw t1
+input_seg=$2      # 0/1 binary segmentation, e.g. GM or WM+GM
+output_prefix=$3  # output 
+axial_slice_number=$4  #Tweak
+axistoslice=$5         #Tweak
 screenshot=${output_prefix}_volRender.jpg
 slice=${output_prefix}_slice${axial_slice_number}.jpg
-ThresholdImage 3 $input_seg seg.nii.gz 1 Inf
-ImageMath 3 $input_seg GetLargestComponent $input_seg
-ImageMath 3 ./biggestConnectedComponent.nii.gz ME $input_seg 1 
-# if [[ ! -s tissueb.nii.gz ]] ; then 
-    Atropos -d 3 -a $input_brain -x $input_seg -o tissue.nii.gz -i kmeans[3] -m [0.1,1x1x1] -c [3,0]
-    ThresholdImage 3 tissue.nii.gz tissueb.nii.gz  2 3 
-    SmoothImage 3  tissueb.nii.gz 0.8 tissueb.nii.gz 
-# fi 
-MultiplyImages 3  ${input_brain} tissueb.nii.gz ./biggestConnectedComponent.nii.gz 
-ExtractSliceFromImage 3 ./biggestConnectedComponent.nii.gz  ${output_prefix}_slice.nii.gz 2 ${axial_slice_number}
-ConvertToJpg ${output_prefix}_slice.nii.gz  $slice 
-c3d ./biggestConnectedComponent.nii.gz -stretch 10% 98% 0 255 -clip 0 255 -o out.vtk
-./bin/volumeRender out.vtk $screenshot 8 0 90 &
+slicem=${output_prefix}_slice${axial_slice_number}_masked.jpg
+cp $input_brain  inbrain.nii.gz 
+ThresholdImage 3 $input_seg seg.nii.gz 1 Inf                      #Tweak
+ImageMath 3 seg.nii.gz GetLargestComponent seg.nii.gz             #Tweak
+ImageMath 3 seg.nii.gz FillHoles seg.nii.gz
+ImageMath 3 ./biggestConnectedComponent.nii.gz ME seg.nii.gz 1    #Tweak
+                                                                  #Tweak segmentation protocol
+Atropos -d 3 -a $input_brain -x seg.nii.gz -o tissue.nii.gz -i kmeans[3] -m [0.1,1x1x1] -c [3,0]
+ThresholdImage 3 tissue.nii.gz tissueb.nii.gz  2 3                #Tweak
+SmoothImage 3  tissueb.nii.gz 0.8 tissueb.nii.gz                  #Tweak
+MultiplyImages 3  inbrain.nii.gz tissueb.nii.gz ./biggestConnectedComponent.nii.gz
+ExtractSliceFromImage 3 inbrain.nii.gz  ${output_prefix}_slice.nii.gz $axistoslice ${axial_slice_number}
+ConvertToJpg ${output_prefix}_slice.nii.gz  $slice
+MultiplyImages 3 inbrain.nii.gz seg.nii.gz  temp.nii.gz
+ExtractSliceFromImage 3 ./temp.nii.gz  ${output_prefix}_slice.nii.gz $axistoslice ${axial_slice_number}
+ConvertToJpg ${output_prefix}_slice.nii.gz  $slicem
+c3d ./biggestConnectedComponent.nii.gz -stretch 10% 98% 0 255 -clip 0 255 -o out.vtk #Tweak 10% higher or lower
+./bin/volumeRender out.vtk $screenshot 8 0 90 &  #Tweak last 3 parameters 
 sleep 10
-rm out.vtk
+rm tissueb.nii.gz tissue.nii.gz out.vtk ${output_prefix}_slice.nii.gz temp.nii.gz seg.nii.gz inbrain.nii.gz ./biggestConnectedComponent.nii.gz 
